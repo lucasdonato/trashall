8f1f5abda5a412ad58f0c7033fe6e6ba806ee243
Lucas Pereira <lucaspolimig96@gmail.com>
2019-10-11 13:45:29 -0300

    layouts

--- pages/admin/pageColetor.php
+++ pages/admin/pageColetor.php
@@ -119,7 +119,10 @@
       <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
         <div class="container-fluid">
           <div class="navbar-wrapper">
-            <a class="navbar-brand" href="#pablo">Cadastrar Coletor</a>
+              <h3>
+                Trashall -
+                <small class="text-muted">Cadastrar Coletores</small>
+              </h3>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
@@ -186,14 +189,14 @@
                 Adicionar</button>
             <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalExcluirColetor">
                 Excluir</button>
-                <button onClick="window.location.reload();"type="button" class="btn btn-ligth">
+                <button onClick="window.location.reload();"type="button" class="btn btn-dark">
                     Recarregar
                 </button>
             
       </div><br><br>
 
-      <table class="table table-striped table-bordered table-hover">
-      <thead class=" text-primary">
+      <table class="table table-striped">
+      <thead>
 
             <?php
                   require_once '../init.php';
@@ -205,26 +208,34 @@
 
                       echo "<div class='card-header card-header-primary'>";
                       echo "<h4 class='card-title'>Coletores cadastrados</h4>";
-                      echo "<p class='card-category'>Listagem</p>";
                       echo "</div>";
-                      echo "<thead class=' text-primary'>";
+                      echo "<thead class='bg-info'>";
                         echo "<th>Nome Coletor</th>";
-                        echo "<th>Data Cadastro</th>";
                         echo "<th>Login</th>";
+                        echo "<th>Contato</th>";
+                        echo "<th>Data Cadastro</th>";
+                        echo "<th>Materiais coletados</th>";
                       echo "</thead>";
                       
                       //constroí a tabela
                       while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        echo "<tr>";
-                          echo " <td class='text-primary'>";
+                        echo "<tr class='table-info'>";
+                          echo " <td>";
                             echo $row['nome_empresa'];
                           echo "</td>";
-                          echo " <td class='text-primary'>";
-                            echo $row['data_cadastro'];
-                          echo "</td>";
-                          echo " <td class='text-primary'>";
+                          echo " <td>";
                             echo $row['login_usuario'];
                           echo "</td>";
+                          echo " <td>";
+                            echo 'resolvercontato';
+                          echo "</td>";
+                          echo " <td>";
+                            echo date('d/m/Y H:i:s',strtotime($row['data_cadastro']));
+                          echo "</td>";
+                          echo " <td>";
+                            echo $row['materiais_coletados'];
+                          echo "</td>";
+                          
                         echo "</tr>";
                       }
 
@@ -236,74 +247,7 @@
       </table>
 
   </div>
-  <div class="fixed-plugin">
-    <div class="dropdown show-dropdown">
-      <a href="#" data-toggle="dropdown">
-        <i class="fa fa-cog fa-2x"> </i>
-      </a>
-      <ul class="dropdown-menu">
-        <li class="header-title"> Sidebar Filters</li>
-        <li class="adjustments-line">
-          <a href="javascript:void(0)" class="switch-trigger active-color">
-            <div class="badge-colors ml-auto mr-auto">
-              <span class="badge filter badge-purple" data-color="azure"></span>
-              <span class="badge filter badge-azure" data-color="azure"></span>
-              <span class="badge filter badge-green" data-color="green"></span>
-              <span class="badge filter badge-warning" data-color="orange"></span>
-              <span class="badge filter badge-danger" data-color="danger"></span>
-              <span class="badge filter badge-rose active" data-color="rose"></span>
-            </div>
-            <div class="clearfix"></div>
-          </a>
-        </li>
-        <li class="header-title">Images</li>
-        <li class="active">
-          <a class="img-holder switch-trigger" href="javascript:void(0)">
-            <img src="../../bootstrap-css-js/assets/img/side.jpg" alt="">
-          </a>
-        </li>
-        <li>
-          <a class="img-holder switch-trigger" href="javascript:void(0)">
-            <img src="../../bootstrap-css-js/assets/img/sidebar-2.jpg" alt="">
-          </a>
-        </li>
-        <li>
-          <a class="img-holder switch-trigger" href="javascript:void(0)">
-            <img src="../../bootstrap-css-js/assets/img/sidebar-3.jpg" alt="">
-          </a>
-        </li>
-        <li>
-          <a class="img-holder switch-trigger" href="javascript:void(0)">
-            <img src="../../bootstrap-css-js/assets/img/sidebar-4.jpg" alt="">
-          </a>
-        </li>
-        <li class="button-container">
-          <a href="https://www.creative-tim.com/product/material-dashboard" target="_blank" class="btn btn-primary btn-block">Free Download</a>
-        </li>
-        <!-- <li class="header-title">Want more components?</li>
-            <li class="button-container">
-                <a href="https://www.creative-tim.com/product/material-dashboard-pro" target="_blank" class="btn btn-warning btn-block">
-                  Get the pro version
-                </a>
-            </li> -->
-        <li class="button-container">
-          <a href="https://demos.creative-tim.com/material-dashboard/docs/2.1/getting-started/introduction.html" target="_blank" class="btn btn-default btn-block">
-            View Documentation
-          </a>
-        </li>
-        <li class="button-container github-star">
-          <a class="github-button" href="https://github.com/creativetimofficial/material-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
-        </li>
-        <li class="header-title">Thank you for 95 shares!</li>
-        <li class="button-container text-center">
-          <button id="twitter" class="btn btn-round btn-twitter"><i class="fa fa-twitter"></i> &middot; 45</button>
-          <button id="facebook" class="btn btn-round btn-facebook"><i class="fa fa-facebook-f"></i> &middot; 50</button>
-          <br>
-          <br>
-        </li>
-      </ul>
-    </div>
-  </div>
+
   <!--   Core JS Files   -->
   <script src="../../bootstrap-css-js/assets/js/core/jquery.min.js"></script>
   <script src="../../bootstrap-css-js/assets/js/core/popper.min.js"></script>
--- pages/bibliotecas.php
+++ pages/bibliotecas.php
@@ -19,3 +19,4 @@
   <link href="../../bootstrap-css-js/css/style.css" rel="stylesheet" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
