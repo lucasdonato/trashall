3c8fb975cad715b4590b7b7f5856d4187b59cc0e
Lucas Pereira <lucaspolimig96@gmail.com>
2019-11-10 00:03:40 -0300

    sdas

--- pages/coleta_vinculada.php
+++ pages/coleta_vinculada.php
@@ -19,41 +19,53 @@
         $stmt->bindParam(':id_solicitacao', $id_solicitacao);
 
             if($stmt->execute()){
-                echo '<table class="table table-striped" cellspacing="0" cellpadding="0">';
-                try{                 
 
-                  echo "<thead>";                  
-                          echo "<th>Data coleta</th>";  
-                          echo "<th>Materiais da coleta</th>"; 
-                          echo "<th>Peso</th>";
-                          echo "<th>Coletor</th>"; 
-                          echo "<th>Contato</th>"; 
+                  /*PEGA A QUANTIDADE DE LINHAS
+                  QUE FORAM ATINGIDAS PELA CONSULTA
+                  PARA VALIDAÇÃO*/
+                  $count = $stmt->rowCount();
 
-                  echo "</thead>";                 
+                  /*SÓ MONTA A TABELA SE EXISTIR
+                  DADOS*/
+                  if($count > 0){
+                        echo '<table class="table table-striped" cellspacing="0" cellpadding="0">';
+                              try{     
+                                    echo "<thead>";                  
+                                          echo "<th>Data coleta</th>";  
+                                          echo "<th>Materiais da coleta</th>"; 
+                                          echo "<th>Peso</th>";
+                                          echo "<th>Coletor</th>"; 
+                                          echo "<th>Contato</th>"; 
+                                    echo "</thead>";               
 
-                  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<tr>";
-                      echo "<td>";
-                            echo date('d/m/Y H:i:s',strtotime($row['data_coleta']));                             
-                      echo "</td>";
-                      echo "<td>";
-                            echo $row['materiais_coletados'];                  
-                      echo "</td>"; echo "<td>";
-                            echo $row['peso']. ' KG';
-                      echo "</td>"; echo "<td>";
-                            echo $row['nome_empresa'];                   
-                      echo "</td>"; echo "<td>";
-                            echo $row['descricao'];               
-                      echo "</td>";
-                    echo "</tr>";
+                                    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                                    echo "<tr>";
+                                    echo "<td>";
+                                          echo date('d/m/Y H:i:s',strtotime($row['data_coleta']));                             
+                                    echo "</td>";
+                                    echo "<td>";
+                                          echo $row['materiais_coletados'];                  
+                                    echo "</td>"; echo "<td>";
+                                          echo $row['peso']. ' KG';
+                                    echo "</td>"; echo "<td>";
+                                          echo $row['nome_empresa'];                   
+                                    echo "</td>"; echo "<td>";
+                                          echo $row['descricao'];               
+                                    echo "</td>";
+                                    echo "</tr>";
+                                    }
+
+                              }catch(PDOException $erro_2){
+                                    echo 'erro'.$erro_2->getMessage();       
+                              } 
+                        echo "</table>"; 
+                  }else{
+                        echo '<div class="alert alert-warning" role="alert">';
+                        echo 'Sem informações de coleta para essa solicitação.';
+                        echo '</div>'; 
                   }
-
-            }catch(PDOException $erro_2){
-              echo 'erro'.$erro_2->getMessage();       
-            } 
-          echo "</table>"; 
+                
         }else{
                 var_dump($stmt->errorInfo());
         }
-
 ?>
