42df4ed08581685a1fb831459a330a14856d8e92
Lucas Pereira <lucaspolimig96@gmail.com>
2019-11-18 22:39:38 -0300

    trashall

--- pages/init.php
+++ pages/init.php
@@ -1,9 +1,9 @@
 <?php
  
 // constantes com as credenciais de acesso ao banco MySQL
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('DB_USER', 'b4f566418cd96d');
+define('DB_PASS', 'f903135e');
 define('DB_NAME', 'db_trashall');
  
 // habilita todas as exibições de erros
